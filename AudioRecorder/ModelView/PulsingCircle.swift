ad956e976094840d461473e4769b856c812889e7
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-25 11:55:23 +0100

    Move constants to specific file

--- AudioRecorder/ModelView/PulsingCircle.swift
+++ AudioRecorder/ModelView/PulsingCircle.swift
@@ -6,46 +6,41 @@
 //  Copyright © 2019 Vinicius Leal. All rights reserved.
 //
 
-import UIKit
+import QuartzCore
 
 class PulsingCircle: CALayer {
-
     var animationGroup = CAAnimationGroup()
-    
+
     var initialPulseScale: Float = 0
     var nextPulseAfter: TimeInterval = 0
     var animationDuration: TimeInterval = 0.8
     var radius: CGFloat = 200
     var numberOfPulses: Float = 1
-    
-    override init(layer: Any) {
-        super.init(layer: layer)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
+
     init(numberOfPulses: Float = 2, radius: CGFloat, position: CGPoint) {
         super.init()
-        
-        self.backgroundColor = UIColor.white.cgColor
-        self.contentsScale = UIScreen.main.scale
+
+        self.backgroundColor = Constants.whiteColor
+        self.contentsScale = Constants.scale
         self.opacity = 0
         self.radius = radius
         self.numberOfPulses = numberOfPulses
         self.position = position
-        
+
         self.bounds = CGRect(x: 0, y: 0, width: radius * 2, height: radius * 2)
         self.cornerRadius = radius
-        
+
         DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
             self.setupAnimationGroup()
-            
+
             DispatchQueue.main.async {
                 self.add(self.animationGroup, forKey: "pulse")
             }
         }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func createScaleAnimation() -> CABasicAnimation {
@@ -53,26 +48,26 @@ class PulsingCircle: CALayer {
         scaleAnimation.fromValue = NSNumber(value: initialPulseScale)
         scaleAnimation.toValue = NSNumber(value: 1)
         scaleAnimation.duration = animationDuration
-        
+
         return scaleAnimation
     }
-    
+
     func createOpacityAnimation() -> CAKeyframeAnimation {
         let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
         opacityAnimation.duration = animationDuration
         opacityAnimation.values = [0.4, 0.8, 0]
         opacityAnimation.keyTimes = [0, 0.2, 1]
-        
+
         return opacityAnimation
     }
-    
+
     func setupAnimationGroup() {
-        self.animationGroup = CAAnimationGroup()
-        self.animationGroup.duration = animationDuration + nextPulseAfter
-        self.animationGroup.repeatCount = numberOfPulses
-        
+        animationGroup = CAAnimationGroup()
+        animationGroup.duration = animationDuration + nextPulseAfter
+        animationGroup.repeatCount = numberOfPulses
+
         let defaultCurve = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
-        self.animationGroup.timingFunction = defaultCurve
-        self.animationGroup.animations = [createScaleAnimation(), createOpacityAnimation()]
+        animationGroup.timingFunction = defaultCurve
+        animationGroup.animations = [createScaleAnimation(), createOpacityAnimation()]
     }
 }
--- AudioRecorder/Supporting Files/Constants.swift
+++ AudioRecorder/Supporting Files/Constants.swift
@@ -6,18 +6,20 @@
 //  Copyright © 2019 Vinicius Leal. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 struct Constants {
-    
-    static let pinkGradientTop = UIColor(red:1.00, green:0.34, blue:0.78, alpha:1.0).cgColor
-    
-    static let pinkGradientBottom = UIColor(red:1.00, green:0.26, blue:0.47, alpha:1.0).cgColor
-    
-    static let pinkColor = UIColor(red:1.00, green:0.26, blue:0.47, alpha:1.0)
-    
-    static let cellPinkColorLeft = UIColor(red:1.00, green:0.26, blue:0.70, alpha:1.0).cgColor
-    
-    static let cellPinkColorRight = UIColor(red:1.00, green:0.26, blue:0.76, alpha:1.0).cgColor
+    static let scale = UIScreen.main.scale
+
+    static let whiteColor = UIColor.white.cgColor
+
+    static let pinkGradientTop = UIColor(red: 1.00, green: 0.34, blue: 0.78, alpha: 1.0).cgColor
+
+    static let pinkGradientBottom = UIColor(red: 1.00, green: 0.26, blue: 0.47, alpha: 1.0).cgColor
+
+    static let pinkColor = UIColor(red: 1.00, green: 0.26, blue: 0.47, alpha: 1.0)
+
+    static let cellPinkColorLeft = UIColor(red: 1.00, green: 0.26, blue: 0.70, alpha: 1.0).cgColor
+
+    static let cellPinkColorRight = UIColor(red: 1.00, green: 0.26, blue: 0.76, alpha: 1.0).cgColor
 }
