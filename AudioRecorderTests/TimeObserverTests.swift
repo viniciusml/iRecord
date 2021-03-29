436982c59a84268626011ce2ed280efc958c7bd9
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-03-29 21:17:13 +0100

    Assert timer is called once

--- AudioRecorderTests/TimeObserverTests.swift
+++ AudioRecorderTests/TimeObserverTests.swift
@@ -15,7 +15,18 @@ protocol ScheduledTimer {
 extension Timer: ScheduledTimer {}
 
 class TimeObserver {
+    private let timeProvider: Timer.Type
+    var observerCallback: () -> Void = {}
     
+    init(timeProvider: Timer.Type = Timer.self) {
+        self.timeProvider = timeProvider
+    }
+    
+    func observe(timeInterval: TimeInterval = 0.02, shouldRepeat: Bool = true) {
+        timeProvider.scheduledTimer(withTimeInterval: timeInterval, repeats: shouldRepeat, block: { _ in
+            self.observerCallback()
+        })
+    }
 }
 
 class TimeObserverTests: XCTestCase {
@@ -27,10 +38,52 @@ class TimeObserverTests: XCTestCase {
         XCTAssertEqual(counter.count, 0)
     }
     
+    func test_observe_startsObserving() {
+        let counter = Counter()
+        let sut = TimeObserver()
+        let exp = expectation(description: "wait for timer to fire")
+        
+        sut.observerCallback = {
+            exp.fulfill()
+            counter.increaseCount()
+        }
+        sut.observe(timeInterval: 1.0)
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(counter.count, 1)
+    }
+    
     // MARK: - Helpers
+    
+    private class TimerSpy: Timer {
+        
+        var block: ((Timer) -> Void)!
+        
+        static var currentTimer: TimerSpy!
+        
+        override func fire() {
+            block(self)
+        }
+        
+        override open class func scheduledTimer(
+            withTimeInterval interval: TimeInterval,
+            repeats: Bool,
+            block: @escaping (Timer) -> Void) -> Timer {
+            
+            let mockTimer = TimerSpy()
+            mockTimer.block = block
+            
+            TimerSpy.currentTimer = mockTimer
+            
+            return mockTimer
+        }
+    }
     
     private class Counter {
         private(set) var count: Int = 0
         
+        func increaseCount() {
+            count += 1
+        }
     }
 }
